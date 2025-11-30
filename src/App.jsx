bf4fd15755978a82797f6432c1c3f20bed3ac0ba
Vaibhav Sathe <71070729+ryucraftz@users.noreply.github.com>
2025-11-30 16:13:07 +0530

    Remove ThreeBackground from App component
    Removed ThreeBackground component from App.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import WallpaperGrid from './components/WallpaperGrid';
 import FeaturedCollections from './components/FeaturedCollections';
-import ThreeBackground from './components/ThreeBackground';
 import Footer from './components/Footer';
 import { SearchProvider } from './context/SearchContext';
 
@@ -11,7 +10,6 @@ function App() {
   return (
     <SearchProvider>
       <div className="app">
-        <ThreeBackground />
         <Header />
         <main>
           <Hero />
